e836a7a6a9ef6545a2288f334d2a0703e6f50e0e
༺֍ｐａｗａｎ֍༻ <109577088+Pawan-1008@users.noreply.github.com>
2023-06-14 10:53:10 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,14 @@
-import 'package:flutter/material.dart';
 // import 'package:flutter/material.dart';
-import 'buttons.dart';
+// // import 'package:flutter/material.dart';
+// import 'buttons.dart';
+
+// void main() {
+//   runApp(Myclass4());
+// }
 
 void main() {
-  runApp(Myclass4());
+  runApp(MaterialApp(initialRoute: '/', routes: {
+    '/': (context) => Myclass26(),
+    '/Second': (context) => Myclass27()
+  }));
 }
